685fabc3c50f040f128d2e8a3824e4174b2aed6c
OmicronPersei <wheeler.weise@gmail.com>
2017-01-21 18:01:57 -0600

    Added more quotes, font-awesome character of the Twitter logo, and some style changes.

--- Javascript.js
+++ Javascript.js
@@ -9,7 +9,13 @@ var getRandomNumber = function(maxVal) {
 
 var quotes = 
   ["We live in a society exquisitely dependent on science and technology, in which hardly anyone knows anything about science and technology.",
-  "If we long to believe that the stars rise and set for us, that we are the reason there is a Universe, does science do us a disservice in deflating our conceits?"];
+  "If we long to believe that the stars rise and set for us, that we are the reason there is a Universe, does science do us a disservice in deflating our conceits?",
+  "If you wish to make an apple pie from scratch, you must first invent the universe.",
+  "We've arranged a civilization in which most crucial elements profoundly depend on science and technology.",
+  "Science is a way of thinking much more than it is a body of knowledge.",
+  "Absence of evidence is not evidence of absence.",
+  "A celibate clergy is an especially good idea, because it tends to suppress any hereditary propensity toward fanaticism.",
+  "The universe is not required to be in perfect harmony with human ambition."];
 
 var getRandomQuote = function(quoteArr) {
   "use strict";
