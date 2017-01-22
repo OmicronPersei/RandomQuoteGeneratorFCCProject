5a160a68f911b49cc2b6f71020b96fff87b473be
OmicronPersei <wheeler.weise@gmail.com>
2017-01-22 17:02:32 -0600

    New quote button now works, and ensures that a different quote is displayed upon each button press.

--- Javascript.js
+++ Javascript.js
@@ -1,12 +1,6 @@
 /*global $, jQuery, ResponsiveBootstrapToolkit*/
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, white: true  */
 
-var getRandomNumber = function(maxVal) {
-  "use strict";
-  
-  return Math.floor(Math.random() * (maxVal + 1));
-};
-
 var quotes = 
   ["We live in a society exquisitely dependent on science and technology, in which hardly anyone knows anything about science and technology.",
   "If we long to believe that the stars rise and set for us, that we are the reason there is a Universe, does science do us a disservice in deflating our conceits?",
@@ -17,10 +11,41 @@ var quotes =
   "A celibate clergy is an especially good idea, because it tends to suppress any hereditary propensity toward fanaticism.",
   "The universe is not required to be in perfect harmony with human ambition."];
 
+var getRandomNumber = function(maxVal) {
+  "use strict";
+  
+  return Math.floor(Math.random() * (maxVal + 1));
+};
+
+var getUniqueRandomNumber = function(maxVal, prevVal)
+{
+  "use strict";
+  
+  if (maxVal > 0)
+    {
+      var nextRandomNumber = prevVal;
+      while (nextRandomNumber === prevVal)
+        {
+          nextRandomNumber = getRandomNumber(maxVal);
+        }
+      
+      return nextRandomNumber;
+    }
+  else
+    {
+      return 0;
+    }
+};
+
+var lastIndexUsed = 0;
+
 var getRandomQuote = function(quoteArr) {
   "use strict";
   
-  return quoteArr[getRandomNumber(quoteArr.length - 1)];
+  var indexToUse = getUniqueRandomNumber((quoteArr.length - 1), lastIndexUsed);
+  lastIndexUsed = indexToUse;
+  
+  return quoteArr[indexToUse];
 };
 
 var setTextToRandomQuote = function() {
@@ -30,6 +55,12 @@ var setTextToRandomQuote = function() {
 };
 
 $(document).ready(function() {
+  "use strict";
+  
+  setTextToRandomQuote();
+});
+
+$("#newQuoteButton").click(function (event) {
   "use strict";
   
   setTextToRandomQuote();
