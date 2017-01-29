341043137903e53cdc5561bf0613df01e8d8ec71
OmicronPersei <wheeler.weise@gmail.com>
2017-01-28 18:29:39 -0600

    Added more quotes.

--- Javascript.js
+++ Javascript.js
@@ -17,10 +17,25 @@ var quotes =
    {"quote": "A celibate clergy is an especially good idea, because it tends to suppress any hereditary propensity toward fanaticism.",
    "author": "Carl Sagan"},
    {"quote": "The universe is not required to be in perfect harmony with human ambition.",
-   "author": "Carl Sagan"}];
+   "author": "Carl Sagan"},
+  {"quote": "Extinction is the rule. Survival is the exception",
+   "author": "Carl Sagan"},
+  {"quote": "Nobody ever figures out what life is all about, and it doesn't matter. Explore the world. Nearly everything is really interesting if you go into it deeply enough.",
+  "author": "Richard Feynman"},
+  {"quote": "We are trying to prove ourselves wrong as quickly as possible, because only in that way can we find progress.",
+  "author": "Richard Feynman"},
+  {"quote": "If you thought that science was certain - well, that is just an error on your part.",
+  "author": "Richard Feynman"},
+  {"quote": "You can know the name of a bird in all the languages of the world, but when you're finished, you'll know absolutely nothing whatever about the bird... So let's look at the bird and see what it's doing -- that's what counts.",
+  "author": "Richard Feynman"},
+  {"quote": "Tea. Earl Grey. Hot.",
+  "author": "Captain Picard"}];
+
 
 var quoteAuthorLinkDictionary = {
-  "Carl Sagan": "https://en.wikipedia.org/w/index.php?title=Carl_Sagan"
+  "Carl Sagan": "https://en.wikipedia.org/w/index.php?title=Carl_Sagan",
+  "Richard Feynman" :"https://en.wikipedia.org/w/index.php?title=Richard_Feynman",
+  "Captain Picard": "https://en.wikipedia.org/wiki/Jean-Luc_Picard"
 };
 
 var quoteAuthor = "Carl Sagan";
